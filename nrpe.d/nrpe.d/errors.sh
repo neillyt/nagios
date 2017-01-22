32a19651ad771140a853ee51534c7c5f29d60a30
root <root@controller.learnwithlinux.com>
2017-01-22 11:00:01 -0500

    Auto backup: Sun Jan 22 11:00:01 EST 2017

--- nrpe.d/nrpe.d/errors.sh
+++ nrpe.d/nrpe.d/errors.sh
@@ -4,8 +4,8 @@ ERROR_COUNT=`tail -1000 /var/log/messages | grep -v "Total errors: 0" | grep -i 
 
 if [ "$ERROR_COUNT" -ge 1 ]; then
  echo "Error count: $ERROR_COUNT"
- exit 2
-else
- echo "Error count: $ERROR_COUNT"
  exit 0
+else
+ echo "Error DERP count: $ERROR_COUNT"
+ exit 2
 fi
